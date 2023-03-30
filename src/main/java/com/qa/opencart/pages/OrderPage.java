017d16f4b000ee44bbe2d72309b1c12a0f431fea
Ashwini Patil <ashupatil02@outlook.com>
2023-03-30 09:22:05 -0400

    added order page file

---
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class OrderPage {
+By loc = By.id("ename");
+public void getOrder() {
+	System.out.println("ordesr page");
+}
+
+
+}
